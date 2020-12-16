bdb7e8674b2aaf89d8445ae562b8461ebbce1be4
provodoihi <profiloes@gmail.com>
2020-12-16 22:34:11 +0700

    Submit practical work 8

---
+++ Practical Work 8/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,67 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    PagerAdapter pagerAdapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+//        Log.i("OnCreate","something");
+
+        //view pager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vpager);
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+//        if( savedInstanceState == null) {
+//            WeatherAndForecastFragment weatherAndForecastFragment = new WeatherAndForecastFragment();
+//            getSupportFragmentManager().beginTransaction().add(R.id.fragment_parent, weatherAndForecastFragment).commit();
+//        }
+    }
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.i("OnStart","something");
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i("OnResume","something");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i("OnPause","something");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.i("OnStop","something");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i("OnDestroy","something");
+    }
+}
